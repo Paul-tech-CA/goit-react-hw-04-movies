19f0b7fb4b359cede27087766eefe40f609ef58b
Pavel Sakun <axelhomkin@gmail.com>
2021-03-06 15:57:45 +0200

    finighed general task need to do refactoring and back home button

--- src/pages/moviesPage/MoviesPage.jsx
+++ src/pages/moviesPage/MoviesPage.jsx
@@ -1,11 +1,65 @@
-import React from 'react';
+import axios from 'axios';
+import React, { Component } from 'react';
+import style from './MoviesPage.module.css';
+import { Link } from 'react-router-dom';
 
-const MoviesPage = () => {
-  return (
-    <div>
-      <h1>Movies page</h1>
-    </div>
-  );
-};
+class MoviesPage extends Component {
+  state = {
+    query: '',
+    movies: [],
+  };
+
+  onHandleChange = event => {
+    this.setState({ query: event.target.value });
+  };
+
+  onHandleSubmit = event => {
+    event.preventDefault();
+    axios
+      .get(
+        `${process.env.REACT_APP_URL}/search/movie?api_key=${
+          process.env.REACT_APP_KEY
+        }&language=en-US&query=${this.state.query}&page=1&include_adult=false`,
+      )
+      .then(response => this.setState({ movies: response.data.results }));
+  };
+  render() {
+    const { query, movies } = this.state;
+    return (
+      <>
+        <form className={style.movieForm} onSubmit={this.onHandleSubmit}>
+          <label className={style.label}>
+            <input
+              type="text"
+              autoComplete="off"
+              autoFocus
+              placeholder="Type the name of movie"
+              name="input"
+              className={style.movieFormInput}
+              value={query}
+              onChange={this.onHandleChange}
+            />
+          </label>
+          <button className={style.buttonSearch} type="submit">
+            Search
+          </button>
+        </form>
+        <>
+          {!movies.length && query.length > 0 ? (
+            <p>Unknown input try one more time</p>
+          ) : (
+            <ul className={style.moviesList}>
+              {movies.map(movie => (
+                <li key={movie.id} className={style.movie}>
+                  <Link to={`/movies/${movie.id}`}>{movie.title}</Link>
+                </li>
+              ))}
+            </ul>
+          )}
+        </>
+      </>
+    );
+  }
+}
 
 export default MoviesPage;
